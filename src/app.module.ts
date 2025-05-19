f7331b45e8404f1d0165fd10e00d06e7517a635f
Fonsi <myo.a@tiltedapp.com>
2025-05-19 15:27:45 +0700

    SET UP PROJECT

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,43 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminModule } from './admin/admin.module';
+import { ShopkeeperModule } from './shopkeeper/shopkeeper.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
+import environmentValidation from './config/environment.validation';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const ENV = process.env.NODE_ENV;
 
 @Module({
-  imports: [],
+  imports: [
+    AdminModule,
+    ShopkeeperModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: !ENV ? '.env.development' : `.env.${ENV}`,
+      load: [appConfig, databaseConfig],
+      validationSchema: environmentValidation,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        autoLoadEntities: configService.get<boolean>(
+          'database.autoLoadEntities',
+        ),
+        synchronize: configService.get<boolean>('database.synchronize'),
+        host: configService.get<string>('database.host'),
+        port: configService.get<number>('database.port'),
+        username: configService.get<string>('database.user'),
+        password: configService.get<string>('database.password'),
+        database: configService.get<string>('database.name'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
