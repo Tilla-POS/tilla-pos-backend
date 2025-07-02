c2c0d3ff849fbb48551b024d5689619a69d17cdb
MyoMyatAung <myatmyo299@gmail.com>
2025-07-02 23:29:05 +0700

    REMOVE ALL FILE

--- src/business/dto/update-business.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateBusinessDto } from './create-business.dto';
-import { IsNotEmpty, IsUUID } from 'class-validator';
-
-export class UpdateBusinessDto extends PartialType(CreateBusinessDto) {
-  @IsUUID()
-  @IsNotEmpty()
-  shopkeeperId: string;
-}
