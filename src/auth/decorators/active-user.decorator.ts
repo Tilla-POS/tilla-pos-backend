e42b51e493ae8604ac3eaff258b53d6f63af5093
MyoMyatAung <myatmyo299@gmail.com>
2025-08-28 12:39:18 +0700

    ACTIVE USER DECORATOR

---
+++ src/auth/decorators/active-user.decorator.ts
@@ -0,0 +1,13 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { ActiveUser as ActiveUserData } from '../interfaces/active-user.inteface';
+import { REQUEST_USER_KEY } from '../constants/auth.constant';
+
+export const ActiveUser = createParamDecorator(
+  (field: keyof ActiveUserData | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const user: ActiveUserData = request[REQUEST_USER_KEY];
+
+    // If a user passes a field to the decorator use only that field
+    return field ? user?.[field] : user;
+  },
+);
